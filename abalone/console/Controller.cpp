adcd99dbbf5fd88680baf8b6de65b1250270fb47
Amine-Ayoub Bigham <aminebigham@hotmail.com>
2021-03-21 21:26:33 +0100

    StartGame controller modif

--- abalone/console/Controller.cpp
+++ abalone/console/Controller.cpp
@@ -10,18 +10,22 @@ Controller::Controller(Game game, ConsoleView cv)
 void Controller::startGame()
 {
     view_.displayWelcome();
+    view_.updateView(game_.getBoard());
+
     while (!game_.isGameOver())
     {
-        view_.updateView(game_.getBoard());
-
         std::string currPlayer = game_.getCurrentPlayer() == Color::BLACK ? "White" : "Black";
-        view_.displayMessage(currPlayer + " is now playing : ");
-
+        view_.displayMessage(currPlayer + " is now playing.");
         auto positions = game_.askAbaPro();
         bool moved = game_.play(positions);
         if(!moved)
-            view_.displayMessage("Could not move, please try again.");
+        {
+            view_.displayMessage("\nCould not move, please try again.");
+        }
         else
+        {
             view_.displayMessage("Move successful ! Next player's turn now.");
+            view_.updateView(game_.getBoard());
+        }
     }
 }
